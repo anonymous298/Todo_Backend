08e441c44b0f0b42a12cb56be90927da19790be6
anonymous298 <tackletalha@gmail.com>
2026-06-17 06:33:46 +0500

    todo route handler created RestAPI routes created

--- src/routes/todo.route.ts
+++ src/routes/todo.route.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
 import type { IRouter } from "express";
-import { handleGetTodo } from "../controllers/todo.controller";
+import { handleCreateTodo, handleDeleteTodo, handleGetTodo, handleUpdateTodo } from "../controllers/todo.controller";
 
 const router: IRouter = Router();
 
 router.get('/', handleGetTodo);
 
+router.post('/', handleCreateTodo);
+
+router.patch('/:id', handleUpdateTodo);
+
+router.delete('/:id', handleDeleteTodo);
 
 export default router;
